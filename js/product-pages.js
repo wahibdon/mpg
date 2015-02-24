7c5fb437a8ddc2baa59347389f403302444163d0
Administrator <whibdon@ottcom.com>
2015-02-24 12:15:24 -0500

    RESULTS PAGE 1. Please make the Brown Foreman logo smaller so it isn't so fuzzy.
    PRODUCTS PAGES
    1. When the portfolio pictures are clicked on, many of them are cut off so you can only see the top of the picture. Please adjust.
    2. On the main page of this section, please add some space between "Point of Purchase" and the video. In addition, add a gray background to either side of the video (should stretch all the way across the screen).
    
    HOME PAGE
    1. When you make the window smaller, is there any way to make it so the black "address bar" at the bottom isn't static and moves with the rest of the page? (let me know if that doesn't make sense).
    
    these things fixed

--- js/product-pages.js
+++ js/product-pages.js
@@ -2,9 +2,11 @@ window.addEventListener('load', function(){
 	$(".img-block > img").each(function(i){
 		$(this).click(function(){
 			var bih= $('#bigImageHolder');
+			var bi = $("#bigImage");
 			bih.show();
 			bih.css('top', window.scrollY+"px");
-			$('#bigImage').attr('src',$(this).attr('src'));
+			bi.attr('src',$(this).attr('src'));
+			bih.css('margin-left', "-"+bih.width()/2+"px");
 		});
 	});
 	$("#closeImage").click(function(){
